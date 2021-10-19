b179e1ff37236a0cf1eb34560b2882cb4ef3a714
wendellunny <wendelferreiraportugal@gmail.com>
2021-10-18 22:58:18 -0300

    Limpar Conteudo

--- robots/text.js
+++ robots/text.js
@@ -2,18 +2,41 @@ const algorithmia = require("algorithmia");
 const algorithmiaApiKey = require("../credentials/algorithmia.json").apiKey;
 const axios = require("axios");
 
-function robot(content){
-    fetchContentFromWikipedia(content);
+async function robot(content){
+    await fetchContentFromWikipedia(content);
     
-    //sanitizeContent(content);
+    sanitizeContent(content);
     //breakContentIntoSentences(content);
 
     async function fetchContentFromWikipedia(content){
-        const apiUrl =`https://pt.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&explaintext&redirects=1&titles=${content.searchTerm}`
+        const apiUrl =`https://en.wikipedia.org/w/api.php?format=json&formatversion=2&action=query&prop=extracts&explaintext&redirects=1&titles=${content.searchTerm}`
         const wikipediaResponse = await axios.get(apiUrl);
-        
         const wikipediaContent = wikipediaResponse.data;
-        console.log(JSON.stringify(wikipediaContent.query.pages));
+        content.sourceContentOriginal = wikipediaContent.query.pages[0].extract
+    }
+
+    function sanitizeContent(content){
+        const withoutBlankLinesAndMarkdown = removeBlankLinesAndMarkdown(content.sourceContentOriginal);
+        const withoutDatesInParentheses = removeDatesInParetheses(withoutBlankLinesAndMarkdown);
+        console.log(withoutDatesInParentheses);
+
+        function removeBlankLinesAndMarkdown(text){
+            const allLines = text.split(`\n`);
+           
+            const withoutBlankLinesAndMarkdown = allLines.filter((line)=>{
+                if(line.trim().length === 0 || line.trim().startsWith('=')){
+                    return false;
+                }
+                return true;
+            })
+
+            return withoutBlankLinesAndMarkdown.join(' ');
+        }
+
+        function removeDatesInParetheses(text){
+            return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm,'').replace(/  /g,' ');
+        }
+
     }
 }
 module.exports = robot;
